82aef81bfb5b2eafb6f2578dd32ff6adf2ee9149
onamonapia <mirrundell@gmail.com>
2024-04-19 14:21:00 -0700

    realised i don't know how errors work in js

--- Movie.js
+++ Movie.js
@@ -18,31 +18,31 @@ let Movie = function(name, rating, year) {
 
 // Checks if the given rating is valid.
 // Returns: True if the given rating is an integer between 1 and 5.
-// Throws: TypeError if the rating is not an integer.
-//         RangeError if the rating is not between 1 and 5.
+// Throws: Error if the rating is not an integer.
+//         Error if the rating is not between 1 and 5.
 let checkRating = function(rating) {
     if (Number.isInteger(rating)) {
         if (rating >= 1 && rating <= 5) {
             return true;
         }
         else {
-            throw new RangeError("Rating must be between 1-5.");
+            throw "Rating must be between 1-5.";
         }
     }
     else {
-        throw new TypeError("Rating is not an integer.");
+        throw "Rating is not an integer.";
     }
 }
 
 // Checks if the given year is valid.
 // Returns: True if the year is an integer.
-// Throws: TypeError if the year is not an integer.
+// Throws: Error if the year is not an integer.
 let checkYear = function(year) {
     if (Number.isInteger(year)) {
         return true;
     }
     else {
-        throw new TypeError("Year is not an integer.");
+        throw "Year is not an integer.";
     }
 }
 
